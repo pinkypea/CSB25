25536785ab7155a11a6cd014564951ba4dee609f
KienDao <daokien2303@gmail.com>
2025-05-17 16:16:06 +0700

    Buổi 1

--- Buoi1.py
+++ Buoi1.py
@@ -5,30 +5,29 @@ x = "MindX"
 print(x)
 print("MindX", "School")
 
-"""
-2. Cách khai báo biến trong python:
-# <tên biến> = <giá trị>
-"""
+
+'''2. Cách khai báo biến trong python: <tên biến> = <giá trị>'''
 name = "Đào Trung Kiên"
 age = 50
 job = "Developer"
 hometown = "Hanoi"
 
-# 3. Các kiểu dữ liệu trong python
-# - Số nguyên: 
-#     Ký hiệu: int(integer)
-#     VD: 3, 4, 10, -100, -2000
-# - Số thực: 
-#     Ký hiệu: float
-#     VD: 3,9; 10,2; 3,14; -1,5
-# - Chuỗi:
-#     Ký hiệu: str(string)
-#     VD: "MindX", 'School', 'Hanoi', "Việt Nam"
-# - Boolean:
-#     Ký hiệu: bool
-#     VD: True, False
-
-# CHÚ Ý: lệnh type() để kiểm tra kiểu dữ liệu của biến
+'''
+3. Các kiểu dữ liệu trong python
+- Số nguyên: 
+    Ký hiệu: int(integer)
+    VD: 3, 4, 10, -100, -2000
+- Số thực: 
+    Ký hiệu: float
+    VD: 3,9; 10,2; 3,14; -1,5
+- Chuỗi:
+    Ký hiệu: str(string)
+    VD: "MindX", 'School', 'Hanoi', "Việt Nam"
+- Boolean:
+    Ký hiệu: bool
+    VD: True, False
+!!! Lệnh type() để kiểm tra kiểu dữ liệu của biến
+'''
 a = 10
 b = 3.14
 c = "Hà Nội"
@@ -37,7 +36,8 @@ print(type(a))
 print(type(b))
 print(type(c))
 print(type(d))
-# 4. Cho phép người dùng nhập từ bàn phím vào chương trình
+
+''' 4. Cho phép người dùng nhập từ bàn phím vào chương trình '''
 chieu_dai = int(input("Nhập chiều dài: "))
 chieu_rong = int(input("Nhập chiều rộng: "))
 
@@ -55,24 +55,26 @@ print(type(b))
 print(type(x))
 print(type(y))
 
-# 5. Các toán tử cơ bản trong python
-# - Cộng: Dùng để tính tổng
-#     Ký hiệu: +
-# - Trừ: Dùng để tính hiệu
-#     Ký hiệu: -
-# - Nhân: Dùng để tính tích
-#     Ký hiệu: *
-# - Chia: Dùng để tính thương
-#     Ký hiệu: /
-# - Lấy phần dư: Dùng để tính phần dư của phép chia
-#     Ký hiệu: %
-#     VD: 7 % 2 = 1, 100 % 30 = 10
-# - Lấy phần nguyên: Dùng để tính phần nguyên của phép chia
-#     Ký hiệu: //
-#     VD: 7 // 2 = 3, 100 // 30 = 3, 10 // 5 = 2
-# - Luỹ thừa: Dùng để tính luỹ thừa
-#     Ký hiệu: **
-#     VD: 2 ** 3 = 8, 10 ** 2 = 100, 5 ** 4 = 625
+'''
+5. Các toán tử cơ bản trong python
+- Cộng: Dùng để tính tổng
+    Ký hiệu: +
+- Trừ: Dùng để tính hiệu
+    Ký hiệu: -
+- Nhân: Dùng để tính tích
+    Ký hiệu: *
+- Chia: Dùng để tính thương
+    Ký hiệu: /
+- Lấy phần dư: Dùng để tính phần dư của phép chia
+    Ký hiệu: %
+    VD: 7 % 2 = 1, 100 % 30 = 10
+- Lấy phần nguyên: Dùng để tính phần nguyên của phép chia
+    Ký hiệu: //
+    VD: 7 // 2 = 3, 100 // 30 = 3, 10 // 5 = 2
+- Luỹ thừa: Dùng để tính luỹ thừa
+    Ký hiệu: **
+    VD: 2 ** 3 = 8, 10 ** 2 = 100, 5 ** 4 = 625
+'''
 
 # Bài tập:
 # 1. Viết chương trình để nhập 2 giá trị là HỌ và TÊN. 
