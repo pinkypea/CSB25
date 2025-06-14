880eaf2158b4a5cd604bd4db674ae5db6773d916
KienDao <daokien2303@gmail.com>
2025-06-14 16:16:48 +0700

    Chương trình Python cơ bản

--- Buoi1.py
+++ Buoi1.py
@@ -1,101 +1,101 @@
-# '''1. Cách in giá trị ra màn hình'''
-# print("Hello world")
-# print(1 + 1)
-# x = "MindX"
-# print(x)
-# print("MindX", "School")
+'''1. Cách in giá trị ra màn hình'''
+print("Hello world")
+print(1 + 1)
+x = "MindX"
+print(x)
+print("MindX", "School")
 
 
-# '''2. Cách khai báo biến trong python: <tên biến> = <giá trị>'''
-# name = "Đào Trung Kiên"
-# age = 50
-# job = "Developer"
-# hometown = "Hanoi"
+'''2. Cách khai báo biến trong python: <tên biến> = <giá trị>'''
+name = "Đào Trung Kiên"
+age = 50
+job = "Developer"
+hometown = "Hanoi"
 
-# '''
-# 3. Các kiểu dữ liệu trong python
-# - Số nguyên: 
-#     Ký hiệu: int(integer)
-#     VD: 3, 4, 10, -100, -2000
-# - Số thực: 
-#     Ký hiệu: float
-#     VD: 3,9; 10,2; 3,14; -1,5
-# - Chuỗi:
-#     Ký hiệu: str(string)
-#     VD: "MindX", 'School', 'Hanoi', "Việt Nam"
-# - Boolean:
-#     Ký hiệu: bool
-#     VD: True, False
-# !!! Lệnh type() để kiểm tra kiểu dữ liệu của biến
-# '''
-# a = 10
-# b = 3.14
-# c = "Hà Nội"
-# d = True
-# print(type(a))
-# print(type(b))
-# print(type(c))
-# print(type(d))
+'''
+3. Các kiểu dữ liệu trong python
+- Số nguyên: 
+    Ký hiệu: int(integer)
+    VD: 3, 4, 10, -100, -2000
+- Số thực: 
+    Ký hiệu: float
+    VD: 3,9; 10,2; 3,14; -1,5
+- Chuỗi:
+    Ký hiệu: str(string)
+    VD: "MindX", 'School', 'Hanoi', "Việt Nam"
+- Boolean:
+    Ký hiệu: bool
+    VD: True, False
+!!! Lệnh type() để kiểm tra kiểu dữ liệu của biến
+'''
+a = 10
+b = 3.14
+c = "Hà Nội"
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
 
-# ''' 4. Cho phép người dùng nhập từ bàn phím vào chương trình '''
-# chieu_dai = int(input("Nhập chiều dài: "))
-# chieu_rong = int(input("Nhập chiều rộng: "))
+''' 4. Cho phép người dùng nhập từ bàn phím vào chương trình '''
+chieu_dai = int(input("Nhập chiều dài: "))
+chieu_rong = int(input("Nhập chiều rộng: "))
 
-# print(type(chieu_dai))
-# print(type(chieu_rong))
+print(type(chieu_dai))
+print(type(chieu_rong))
 
-# # Phương pháp ép kiểu dữ liệu
-# a = 10 # int
-# b = "10" # str
-# x = float(a)
-# y = int(b)
+# Phương pháp ép kiểu dữ liệu
+a = 10 # int
+b = "10" # str
+x = float(a)
+y = int(b)
 
-# print(type(a))
-# print(type(b))
-# print(type(x))
-# print(type(y))
+print(type(a))
+print(type(b))
+print(type(x))
+print(type(y))
 
-# '''
-# 5. Các toán tử cơ bản trong python
-# - Cộng: Dùng để tính tổng
-#     Ký hiệu: +
-# - Trừ: Dùng để tính hiệu
-#     Ký hiệu: -
-# - Nhân: Dùng để tính tích
-#     Ký hiệu: *
-# - Chia: Dùng để tính thương
-#     Ký hiệu: /
-# - Lấy phần dư: Dùng để tính phần dư của phép chia
-#     Ký hiệu: %
-#     VD: 7 % 2 = 1, 100 % 30 = 10
-# - Lấy phần nguyên: Dùng để tính phần nguyên của phép chia
-#     Ký hiệu: //
-#     VD: 7 // 2 = 3, 100 // 30 = 3, 10 // 5 = 2
-# - Luỹ thừa: Dùng để tính luỹ thừa
-#     Ký hiệu: **
-#     VD: 2 ** 3 = 8, 10 ** 2 = 100, 5 ** 4 = 625
-# '''
+'''
+5. Các toán tử cơ bản trong python
+- Cộng: Dùng để tính tổng
+    Ký hiệu: +
+- Trừ: Dùng để tính hiệu
+    Ký hiệu: -
+- Nhân: Dùng để tính tích
+    Ký hiệu: *
+- Chia: Dùng để tính thương
+    Ký hiệu: /
+- Lấy phần dư: Dùng để tính phần dư của phép chia
+    Ký hiệu: %
+    VD: 7 % 2 = 1, 100 % 30 = 10
+- Lấy phần nguyên: Dùng để tính phần nguyên của phép chia
+    Ký hiệu: //
+    VD: 7 // 2 = 3, 100 // 30 = 3, 10 // 5 = 2
+- Luỹ thừa: Dùng để tính luỹ thừa
+    Ký hiệu: **
+    VD: 2 ** 3 = 8, 10 ** 2 = 100, 5 ** 4 = 625
+'''
 
-# # Bài tập:
-# # 1. Viết chương trình để nhập 2 giá trị là HỌ và TÊN. 
-# #     Sau đó in 2 giá trị ra màn hình cùng 1 dòng.
+# Bài tập:
+# 1. Viết chương trình để nhập 2 giá trị là HỌ và TÊN. 
+#     Sau đó in 2 giá trị ra màn hình cùng 1 dòng.
 
-# ho = input("Họ: ")
-# ten = input("Tên: ")
-# print(ho, ten)
+ho = input("Họ: ")
+ten = input("Tên: ")
+print(ho, ten)
 
-# # 2. Viết chương trình để nhập vào 2 giá trị là a và b.
-# #     Sau đó sử dụng 7 toán tử bên trên để tính các giá trị của chúng.
+# 2. Viết chương trình để nhập vào 2 giá trị là a và b.
+#     Sau đó sử dụng 7 toán tử bên trên để tính các giá trị của chúng.
 
-# a = int(input("Nhập a: "))
-# b = int(input("Nhập b: "))
-# print("a + b =", a + b)
-# print("a - b =", a - b)
-# print("a * b =", a * b)
-# print("a / b =", a / b)
-# print("a %", "b =", a % b)
-# print("a // b =", a // b)
-# print("a^b =", a ** b)
+a = int(input("Nhập a: "))
+b = int(input("Nhập b: "))
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+print("a %", "b =", a % b)
+print("a // b =", a // b)
+print("a^b =", a ** b)
 
 
 # Chữa BTVN
